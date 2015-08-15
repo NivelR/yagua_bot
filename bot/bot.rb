475c58d903a4311a9cdc0d79831e65be6c0ad4c6
Matias M <matiasmasca@gmail.com>
2015-08-15 16:12:53 -0300

    Mensajes personalizados, captura ip

--- bot/bot.rb
+++ bot/bot.rb
@@ -22,24 +22,30 @@ bot.run do |client, message|
     ]
   )
 
+  #if message.text =~ /^(\d+\.\d+\.\d+.\d+)/;
+  #	client.send_chat_action(chat_id: message.chat.id, action: :typing)
+  # client.send_message(chat_id: message.chat.id, text: "Enviaste una IP. woof")
+  #end
+
   case message.text
   when /start/
     msg = "Woff arf arf"
     client.send_message(chat_id: message.chat.id, text: msg, reply_markup: markup)
   
   when /agregar_server/
-  	 msg = "Agregado, quedate piola #{message.text}. woof"
-  	client.send_message(chat_id: message.chat.id, text: msg)
-  	sleep 3
-    client.send_chat_action(chat_id: message.chat.id, action: :typing)
-    client.send_message(chat_id: message.chat.id, text: "Listo. woof")
+  	client.send_chat_action(chat_id: message.chat.id, action: :typing)
+  	client.send_message(chat_id: message.chat.id, text: "Decime la IP:. -woof")
+    sleep 1  	    
   
   when /quien_es_trolo/
     msg = "Silvio llegó último a GitHub"
     client.send_message(chat_id: message.chat.id, text: msg, reply_markup: markup)
-  
+  when /^(\d+\.\d+\.\d+.\d+)/
+  	client.send_chat_action(chat_id: message.chat.id, action: :typing)
+    client.send_message(chat_id: message.chat.id, text: "Enviaste una IP: #{message.text}. -woof")
+
   else
-    msg = "Dedicate a otra cosa... no te entiendo lo que decis... woof"
+    msg = "Dedicate a otra cosa... no te entiendo lo que decis... -woof"
     client.send_message(chat_id: message.chat.id, text: msg, reply_markup: markup)
   end
 end
