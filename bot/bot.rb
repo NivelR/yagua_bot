f1edc08bb71ce70cbe7e75cfede058a6fd194a60
Agustin N. R. Ramirez <agustin107@gmail.com>
2015-08-15 18:25:24 -0300

    Agrega modelo y mapper

--- bot/bot.rb
+++ bot/bot.rb
@@ -1,8 +1,15 @@
 #!/usr/bin/env ruby
 require 'telebot'
 require 'pp'
-require 'net/http' 
-require_relative 'grapher' 
+require 'net/http'
+require 'mongo_mapper'
+require_relative 'grapher'
+
+Dir['../model/**/*.rb'].sort.each          { |rb| require rb }
+
+
+MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
+MongoMapper.database = "yagua_bot"
 
 token =
   if ENV['TOKEN']
@@ -42,8 +49,11 @@ bot.run do |client, message|
 
 	  when /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/
 	  	client.send_chat_action(chat_id: message.chat.id, action: :typing)
-	    client.send_message(chat_id: message.chat.id, text: "Enviaste una IP: #{message.text}. -woof")
+	    client.send_message(chat_id: message.chat.id, text: "Enviaste una IP a: #{message.text}. -woof")
 	    @ip = message.text
+      server_id = Server.all.count.to_s(16)
+      @server = Server.new(:server_id => server_id, :name => message.text, :ip => message.text, :port => 9999)
+      @server.save
 	  	client.send_chat_action(chat_id: message.chat.id, action: :typing)
 	  	client.send_message(chat_id: message.chat.id, text: "Decime el puerto: /port 9393 -woof")
 
